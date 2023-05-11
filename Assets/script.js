77e99843ed1da695bbe43218a2933e0e97a82056
k-pineda <karenp9310@gmail.com>
2023-05-10 21:38:18 -0400

    added ul to questions container & saved data from category 1 to question and answers container

--- Assets/script.js
+++ Assets/script.js
@@ -1,40 +1,63 @@
-// questions variable array goes here
-$(document).ready(function () {
-
-    var scienceTriviaAPI = `https://opentdb.com/api.php?amount=10&category=17&difficulty=easy`
-
-
+// api variables
+var scienceTriviaAPI = `https://opentdb.com/api.php?amount=10&category=17&difficulty=easy`
 var randomJeapordyAPI=`http://jservice.io/api/random?count=5`
 
-    // setItem local storage will go here 
-    // By Frank on 9th May 2023:
-    // set local storage logic for input user enters in input element
-    // need to use  localStorage.setItem() and localStorage.getItem()
+// element variables
+var questionCategory1=$(".question-title")
+var answerChoices=$(".answer-choices")
 
-    // get input from email text box and do localstorage.setitem with that variable with input value
-    var email_text_input = $('#exampleInputEmail1').val();
-    console.log(email_text_input);
-    localStorage.setItem("exampleInputEmail1", email_text_input);
+// current question index
+var currentIndex = 0;
 
-    // write if & for loop statement for category 1 here 
+// write if & for loop statement for category 1 here (answers will be in multiple choice)
 
-    // write if & for loop statement for category 2 here 
+function getTriviaAPI(requestURL) {
+    fetch(requestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) 
+        {
+                //grabbing options neeed to add these to li elements before appending li element to UL
+                var answer =  data.results[currentIndex].incorrect_answers[0]
+                var answer2 =  data.results[currentIndex].incorrect_answers[1]
+                var answer3 =  data.results[currentIndex].incorrect_answers[2]
+                var correctAnswer = data.results[currentIndex].correct_answer
 
-    function getTriviaAPI(requestURL) {
-        fetch(requestURL)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data)
-            })
-    }
+                // inserting question into element
+                questionCategory1.text(data.results[currentIndex].question)
 
-    $("#category_1").on("click", function (event) {
-        getTriviaAPI(scienceTriviaAPI);
-    })
+                // create list item
+                var firstOptionEl=$("<li>", {id:"option_1"});
+                var secondOptionEl=$("<li>", {id:"option_2"});
+                var thirdOptionEl=$("<li>", {id:"option_3"});
+                var fourthOptionEl=$("<li>", {id:"option_4"});
 
-    $("#category_2").on("click", function (event) {
-        getTriviaAPI(randomJeapordyAPI);
-    })
-});
+                // insert text to li here?????
+                firstOptionEl.text(answer);
+                secondOptionEl.text(answer2);
+                thirdOptionEl.text(answer3);
+                fourthOptionEl.text(correctAnswer);
+
+                // then append to ul
+                answerChoices.append(firstOptionEl);
+                answerChoices.append(secondOptionEl);
+                answerChoices.append(thirdOptionEl);
+                answerChoices.append(fourthOptionEl); 
+        })
+}
+
+// write if & for loop statement for category 2 here (answers will be in text box)
+
+$("#category_1").on("click", function (event) {
+    event.preventDefault()
+    $(".first-page").attr("class", "hide")
+    $(".questions-container").removeAttr("class", "hide")
+    getTriviaAPI(scienceTriviaAPI);
+})
+
+// $("#category_2").on("click", function (event) {
+//     getTriviaAPI(randomJeapordyAPI);
+// })
+
+
